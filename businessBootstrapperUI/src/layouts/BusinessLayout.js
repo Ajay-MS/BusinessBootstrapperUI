242a2fdd0e0dfb95489b64d64fcb887a0f2d8d75
Prachi Chouksey <prchouks@microsoft.com>
2022-03-24 19:27:05 +0530

    adding some changes for routing

--- businessBootstrapperUI/src/layouts/BusinessLayout.js
+++ businessBootstrapperUI/src/layouts/BusinessLayout.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { BrowserRouter, Route, Switch, Redirect, Link } from "react-router-dom";
+import "../assets/css/button.css";
+
+
 
 // react-bootstrap components
 import {
@@ -16,6 +20,11 @@ import {
 function BusinessLayout() {
   return (
     <>
+     <div id='button'>
+     <Link to="/form">
+    <button> Create</button>
+    </Link>
+  </div>
       <Container fluid>
         <Row>
           <Col md="12">
@@ -40,25 +49,35 @@ function BusinessLayout() {
                       <td>1</td>
                       <td>FHLecommerce</td>
                       <td>Ecommerce</td>
-                      <td><Button className="btn-fill" variant="primary">View</Button ></td>
+                      <td>
+                      <Link to="/admin">
+                        <Button className="btn-fill" variant="primary" >View</Button >
+                        </Link>
+                       </td>
                     </tr>
                     <tr>
                       <td>2</td>
                       <td>FHLstream</td>
                       <td>Streaming</td>
-                      <td><Button className="btn-fill" variant="primary">View</Button ></td>
+                      <Link to="/admin">
+                      <td><Button className="btn-fill" variant="primary" >View</Button ></td>
+                      </Link>
                     </tr>
                     <tr>
                       <td>3</td>
                       <td>FHLwallet</td>
                       <td>Wallet</td>
+                      <Link to="/admin">
                       <td><Button className="btn-fill" variant="primary">View</Button ></td>
+                      </Link>
                     </tr>
                     <tr>
                       <td>3</td>
                       <td>FHLchat</td>
                       <td>Messenger</td>
-                      <td><Button className="btn-fill" variant="primary">View</Button ></td>
+                      <Link to="/admin">
+                      <td><Button className="btn-fill" variant="primary" >View</Button ></td>
+                      </Link>
                     </tr>
                   </tbody>
                 </Table>
