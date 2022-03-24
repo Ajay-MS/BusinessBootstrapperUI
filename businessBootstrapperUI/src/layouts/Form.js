189f1d0c55fc586ffa55b28526b66475adec7029
Prachi Chouksey <prchouks@microsoft.com>
2022-03-24 20:34:01 +0530

    more changes for form creation

--- businessBootstrapperUI/src/layouts/Form.js
+++ businessBootstrapperUI/src/layouts/Form.js
@@ -1,46 +1,98 @@
-import React from "react";
-class FormLayout extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-      }
-    
-  
-      render() {
-        return (
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Business Name:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <label>
-              Address:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <label>
-              Contact
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-        );
-      }
-    }
-  
+import React from 'react'
+import { render } from 'react-dom'
+import Styles from '../assets/Styles/Styles'
+import { Form, Field } from 'react-final-form'
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const onSubmit = async values => {
+  await sleep(300)
+  window.alert(JSON.stringify(values, 0, 2))
+}
+
+const FormLayout = () => (
+  <Styles>
+    <h1>Create your businesses</h1>
+    <a
+      href="https://final-form.org/react"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Read Docs
+    </a>
+    <Form
+      onSubmit={onSubmit}
+      render={({ handleSubmit, form, submitting, pristine, values }) => (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Business Name</label>
+            <Field
+              name="tName"
+              component="input"
+              type="text"
+              placeholder=""
+            />
+          </div>     
+          <div>
+            <label>Sauces</label>
+            <div>
+              <label>
+                <Field
+                  name="sauces"
+                  component="input"
+                  type="checkbox"
+                  value="URL Shortener"
+                />{' '}
+                Ketchup
+              </label>
+              <label>
+                <Field
+                  name="sauces"
+                  component="input"
+                  type="checkbox"
+                  value="ECommerce"
+                />{' '}
+                Mustard
+              </label>
+              <label>
+                <Field
+                  name="sauces"
+                  component="input"
+                  type="checkbox"
+                  value="mayonnaise"
+                />{' '}
+                Mayonnaise
+              </label>
+              <label>
+                <Field
+                  name="sauces"
+                  component="input"
+                  type="checkbox"
+                  value="guacamole"
+                />{' '}
+                Guacamole 🥑
+              </label>
+            </div>
+          </div>
+         
+          <div className="buttons">
+            <button type="submit" disabled={submitting || pristine}>
+              Submit
+            </button>
+            <button
+              type="button"
+              onClick={form.reset}
+              disabled={submitting || pristine}
+            >
+              Reset
+            </button>
+          </div>
+          <pre>{JSON.stringify(values, 0, 2)}</pre>
+        </form>
+      )}
+    />
+  </Styles>
+)
 
 
-export default FormLayout;
-
-
+  export default FormLayout;
